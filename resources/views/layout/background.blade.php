871a17f0f63a3a266c8ac536ac04510410944274
safrizal <safrizalhk12@gmail.com>
2024-12-11 01:10:01 +0700

    first commit

---
+++ resources/views/layout/background.blade.php
@@ -0,0 +1,21 @@
+<div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('./assets/img/bg2.jpg');">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand">
+            <h1>Material Kit.</h1>
+            <h3>A Badass Bootstrap 4 UI Kit based on Material Design.</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main main-raised">
+    <div class="section section-basic">
+      <div class="container">
+        <div class="title">
+          <h2>Basic Elements</h2>
+        </div>
+      </div>
+    </div>
+  </div>   
